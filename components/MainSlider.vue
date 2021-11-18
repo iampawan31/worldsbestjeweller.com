ce0e135834d112ed2a6ca1f4e4bdb97ab0fe195a
Pawan Kumar <surpawan@gmail.com>
2021-11-18 12:47:29 +0530

    Added Text to slides

--- components/MainSlider.vue
+++ components/MainSlider.vue
@@ -4,30 +4,37 @@
       class="w-full swiper h-72 md:h-96 lg:h-screen"
       :options="swiperOption"
     >
-      <swiper-slide
-        ><img
-          class="w-full h-72 md:h-96 lg:h-screen object-cover"
-          src="/slides/1.jpg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          class="w-full h-72 md:h-96 lg:h-screen object-cover"
-          src="/slides/2.jpg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          class="w-full h-72 md:h-96 lg:h-screen object-cover"
-          src="/slides/3.jpg"
-          alt=""
-      /></swiper-slide>
-      <swiper-slide
-        ><img
-          class="w-full h-72 md:h-96 lg:h-screen object-cover"
-          src="/slides/4.jpg"
-          alt=""
-      /></swiper-slide>
+      <swiper-slide v-for="(slide, index) in slideImages" :key="index">
+        <div>
+          <img
+            class="w-full h-72 md:h-96 lg:h-screen object-cover"
+            :src="slide.image"
+            alt=""
+          />
+        </div>
+        <div
+          class="
+            text-white
+            lg:bg-white lg:bg-opacity-30
+            bg-black bg-opacity-70
+            p-1
+            absolute
+            bottom-10
+            lg:top-10
+            left-2
+            lg:left-10
+            md:text-2xl
+            lg:text-5xl
+            max-w-xs
+            md:max-w-xl
+            lg:max-w-5xl
+            uppercase
+            font-bold
+          "
+        >
+          {{ slide.text }}
+        </div></swiper-slide
+      >
       <div slot="pagination" class="swiper-pagination"></div>
     </swiper>
   </client-only>
@@ -40,16 +47,49 @@ export default {
     return {
       swiperOption: {
         loop: true,
-        speed: 600,
+        speed: 2000,
         autoplay: {
           delay: 4000,
         },
-        effecct: 'slide',
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true,
+        },
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
         },
       },
+      slideImages: [
+        {
+          image: '/slides/1.jpg',
+          text: 'Jeweller to the royal families of the world since 1807',
+        },
+        {
+          image: '/slides/2.jpg',
+          text: 'regal estate jewellery from the treasure troves',
+        },
+        {
+          image: '/slides/3.jpg',
+          text: 'splendid artistry',
+        },
+        {
+          image: '/slides/4.jpg',
+          text: 'intricate & timeless craftsmanship',
+        },
+        {
+          image: '/slides/5.jpg',
+          text: 'delicate luxury',
+        },
+        {
+          image: '/slides/6.jpg',
+          text: 'jewellery that is your crowning glory',
+        },
+        {
+          image: '/slides/7.jpg',
+          text: 'masters of dreams',
+        },
+      ],
     }
   },
 }
