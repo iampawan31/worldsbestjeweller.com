2f470d4d51f18f4de8b9daedb9b7c5b0760969d5
Pawan Kumar <surpawan@gmail.com>
2021-11-21 21:29:03 +0530

    Added Form

---
+++ components/ContactForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <form action="https://formspree.io/f/mayargre" method="post">
+    <div class="mb-4">
+      <label for="name" class="text-xl">Name:</label>
+      <br />
+      <input
+        class="rounded-md w-full p-4 bg-gray focus:border-primary"
+        type="text"
+        name="name"
+        placeholder="John Doe"
+      />
+    </div>
+    <div class="my-4">
+      <label for="name" class="text-xl">Email:</label>
+      <br />
+      <input
+        class="rounded-md w-full p-4 bg-gray focus:border-primary"
+        type="email"
+        name="email"
+        placeholder="john@doe.com"
+      />
+    </div>
+    <div class="my-4">
+      <label for="phone">Phone:</label>
+      <br />
+      <input
+        class="rounded-md w-full p-4 bg-gray focus:border-primary"
+        type="text"
+        name="phone"
+        placeholder="+91 XXXXXXXXXX"
+      />
+    </div>
+    <div class="my-4">
+      <label for="message" class="text-xl">Message:</label>
+      <br />
+      <textarea
+        class="rounded-md w-full p-4 bg-gray focus:border-primary"
+        row="100"
+        name="message"
+        placeholder="Message here..."
+      />
+    </div>
+    <div class="my-4">
+      <div class="g-recaptcha" data-sitekey="googleCaptchaKey"></div>
+    </div>
+    <div>
+      <button
+        type="submit"
+        class="
+          px-16
+          py-4
+          bg-black
+          text-primary
+          hover:text-black
+          rounded-md
+          block
+          w-full
+          uppercase
+          transition
+          hover:bg-primary
+        "
+      >
+        Send
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  computed: {
+    googleCaptchaKey() {
+      return this.process.env.NUXT_ENV_CAPTCHA_KEY
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -28,67 +28,7 @@
           <div class="text-2xl sm:text-3xl lg:text-4xl font-light mb-10">
             Leave us a message here:
           </div>
-          <form action="" method="post">
-            <div class="mb-4">
-              <label for="name" class="text-xl">Name:</label>
-              <br />
-              <input
-                class="rounded-md w-full p-4 bg-gray focus:border-primary"
-                type="text"
-                name="name"
-                placeholder="John Doe"
-              />
-            </div>
-            <div class="my-4">
-              <label for="name" class="text-xl">Email:</label>
-              <br />
-              <input
-                class="rounded-md w-full p-4 bg-gray focus:border-primary"
-                type="email"
-                name="email"
-                placeholder="john@doe.com"
-              />
-            </div>
-            <div class="my-4">
-              <label for="phone">Phone:</label>
-              <br />
-              <input
-                class="rounded-md w-full p-4 bg-gray focus:border-primary"
-                type="text"
-                name="phone"
-                placeholder="+91 XXXXXXXXXX"
-              />
-            </div>
-            <div class="my-4">
-              <label for="message" class="text-xl">Message:</label>
-              <br />
-              <textarea
-                class="rounded-md w-full p-4 bg-gray focus:border-primary"
-                row="100"
-                name="message"
-                placeholder="Message here..."
-              />
-            </div>
-            <div>
-              <button
-                class="
-                  px-16
-                  py-4
-                  bg-black
-                  text-primary
-                  hover:text-black
-                  rounded-md
-                  block
-                  w-full
-                  uppercase
-                  transition
-                  hover:bg-primary
-                "
-              >
-                Send
-              </button>
-            </div>
-          </form>
+          <ContactForm />
         </div>
       </div>
     </div>
